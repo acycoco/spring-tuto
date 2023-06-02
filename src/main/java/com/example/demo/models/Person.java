e60ab1a38448aeed880fccfb2b9798dbd9f4c2f6
acycoco <pink10257@naver.com>
2023-06-02 15:47:10 +0900

    feat: web ui 연결, customer 목록

---
+++ src/main/java/com/example/demo/models/Person.java
@@ -0,0 +1,31 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+    @Id //이거는 id다.
+    @GeneratedValue(strategy = GenerationType.AUTO) //알아서 관리
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
